68ca3fd4547897a2688e26863e2a71c42296912d
DESKTOP-SHK9PBH\olegk <kornyakovo@gmail.com>
2023-07-18 17:40:45 +0400

    photos init

--- src/App.js
+++ src/App.js
@@ -1,84 +1,50 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Block } from "./Block";
-import "./index.scss";
+import React from 'react';
+import './index.scss';
+
+function Collection({ name, images }) {
+  return (
+    <div className="collection">
+      <img className="collection__big" src={images[0]} alt="Item" />
+      <div className="collection__bottom">
+        <img className="collection__mini" src={images[1]} alt="Item" />
+        <img className="collection__mini" src={images[2]} alt="Item" />
+        <img className="collection__mini" src={images[3]} alt="Item" />
+      </div>
+      <h4>{name}</h4>
+    </div>
+  );
+}
 
 function App() {
-  const [fromCurrency, setFromCurrence] = useState("RUB");
-  const [toCurrence, setToCurrence] = useState("USD");
-  // const [ratesRef.current, setratesRef.current] = useState({});
-  const ratesRef = useRef({})
-  const [fromPrice, setFromPrice] = useState(0);
-  const [toPrice, setToPrice] = useState(1);
-
-  const RUB = {
-    CharCode: "RUB",
-    ID: "RUB1",
-    Name: "Рубль",
-    Nominal: 1,
-    NumCode: "000",
-    Previous: 1,
-    Value: 1,
-  };
-
-  useEffect(() => {
-    fetch("https://www.cbr-xml-daily.ru/daily_json.js")
-      .then((res) => res.json())
-      .then((json) => {
-        // setratesRef.current({ ...json.Valute, RUB });
-        ratesRef.current = { ...json.Valute, RUB }
-        onChangeToPice(1)
-      })
-      .catch((err) => {
-        console.warn(err);
-        alert("Не удалось получить данные");
-      });
-  }, []);
-
-  const onChangeFromPice = (value) => {
-    const convertNominalToCurrence =
-      ratesRef.current[toCurrence].Value / ratesRef.current[toCurrence].Nominal;
-    const convertNominalFromCurrence =
-      ratesRef.current[fromCurrency].Value / ratesRef.current[fromCurrency].Nominal;
-    const price = value / convertNominalToCurrence;
-    const result = price * convertNominalFromCurrence;
-
-    setFromPrice(value);
-    setToPrice(result.toFixed(3));
-  };
-  const onChangeToPice = (value) => {
-    const convertNominalToCurrence =
-      ratesRef.current[toCurrence].Value / ratesRef.current[toCurrence].Nominal;
-    const convertNominalFromCurrence =
-      ratesRef.current[fromCurrency].Value / ratesRef.current[fromCurrency].Nominal;
-    const result =
-      (convertNominalToCurrence / convertNominalFromCurrence) * value;
-
-    setFromPrice(result.toFixed(3));
-    setToPrice(value);
-  };
-
-  useEffect(() => {
-    if (Object.keys(ratesRef.current).length !== 0) onChangeFromPice(fromPrice);
-  }, [fromCurrency]);
-
-  useEffect(() => {
-    if (Object.keys(ratesRef.current).length !== 0) onChangeToPice(toPrice);
-  }, [toCurrence]);
-
   return (
     <div className="App">
-      <Block
-        value={fromPrice}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrence}
-        onChangeValue={onChangeFromPice}
-      />
-      <Block
-        value={toPrice}
-        currency={toCurrence}
-        onChangeCurrency={setToCurrence}
-        onChangeValue={onChangeToPice}
-      />
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          <li className="active">Все</li>
+          <li>Горы</li>
+          <li>Море</li>
+          <li>Архитектура</li>
+          <li>Города</li>
+        </ul>
+        <input className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        <Collection
+          name="Путешествие по миру"
+          images={[
+            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          ]}
+        />
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
